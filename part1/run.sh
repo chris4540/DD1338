77b762fe0ccaa6808b8c4fbc59b9ad8deb16faa0
Chris Lin <chris4540@gmail.com>
2019-07-11 00:49:04 +0800

    Add selection sorting

---
+++ part1/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export LC_ALL="ja_JP.SJIS@cjknarrow"
+javac selectionSortExample.java
+java selectionSortExample
---
+++ part1/selectionSortExample.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+class selectionSortExample {
+	public static void main(String args[])
+	{
+		int[] lst = {4,9,7,1,3,6,5};
+		System.out.println("Before sorting: ");
+		System.out.println(Arrays.toString(lst));
+
+		// sort the list
+		selectionSort(lst);
+		System.out.println("After sorting: ");
+		System.out.println(Arrays.toString(lst));
+
+	}
+
+	/*
+	* Selection sort
+	* Approx. execution: n + (n-1) + ... + 1 = n(n-1)/2
+	* The time complexity = O(n**2)
+	*/
+	static void selectionSort(int[] lst) {
+		// get the length
+		int n = lst.length;
+		for (int i = 0; i < n; i++) {
+			int index = 0;
+			int smallest = lst[i];
+			for (int j = i; j < n; j++) {
+				if (lst[j] < smallest) {
+					smallest = lst[j];
+					index = j;
+				}
+			int temp = lst[i];
+			lst[i] = smallest;
+			lst[index] = temp;
+		 	}
+	   }
+
+	}
+}
+
