dc1575ec8cda350f0de33a4758bbc11d66c2c3da
Chris Lin <chris4540@gmail.com>
2019-07-17 00:55:46 +0800

    Add some modification

--- part2/sorting/quicksort.cpp
+++ part2/sorting/quicksort.cpp
@@ -1,7 +1,19 @@
 // The another implementation in C+=
 #include <iostream>
+#include <vector>
 using namespace std;
 
+vector<string> getStringArr(){
+    vector<string> ret{"Blue", "Red", "Orange", "Yellow"};
+    return ret;
+}
+
 int main(){
-    cout << "Helloworld" << endl;
+
+    // Initialize String Array
+    vector<string> colour = getStringArr();
+
+    // Print Strings
+    for (int i = 0; i < 4; i++)
+        cout << colour[i] << "\n";
 }
