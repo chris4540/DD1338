3a49b50296e298cebc465e81fb739fa077d0e1da
Chris Lin <chris4540@gmail.com>
2019-07-18 11:02:23 +0800

    fix lib

--- part2/lib/utils.hpp
+++ part2/lib/utils.hpp
@@ -2,4 +2,16 @@
 #include <vector>
 
 template <typename T>
-void printVec(std::vector<T>& vec);
+void printVec(std::vector<T>& vec){
+    int size = vec.size();
+    std::cout << "[";
+    for (size_t i=0; i < size-1; ++i){
+        T e = vec[i];
+        std::cout << e;
+        std::cout << ", ";
+    }
+    // print the last element
+    std::cout << vec[size-1];
+    std::cout << "]";
+    std::cout << std::endl;
+}
